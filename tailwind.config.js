eb63811ee2d40ed506a973385f608d467203bace
tantivarakorn <tantivara@gmail.com>
2022-09-30 14:14:02 +0700

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			screens: {
+				lg: '1080px',
+			},
+			fontFamily: {
+				Roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/line-clamp')],
 };
